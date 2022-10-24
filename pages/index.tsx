238ebeb621b54e020eb8d585ccf48459cfaba4af
Amoong <moo1323@naver.com>
2022-10-24 19:42:16 +0900

    Responsive Modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,99 +2,15 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
-      <div className="bg-white p-6 rounded-2xl shadow-xl">
-        <span className="font-semibold text-3xl">Select Item</span>
-        <ul>
-          {[1, 2, 3, 4, 5].map((i) => (
-            <div
-              key={i}
-              className="flex justify-between my-2 odd:bg-blue-500 even:bg-yellow-500"
-            >
-              <span className="text-gray-500">Gray Chair</span>
-              <span className="font-semibold">$19</span>
-            </div>
-          ))}
-        </ul>
-        <ul>
-          {["a", "b", "c", ""].map((c, i) => (
-            <li className="bg-red-500 py-2 empty:hidden" key={i}>
-              {c}
-            </li>
-          ))}
-        </ul>
-        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
-          <span>Total</span>
-          <span className="font-semibold">$10</span>
-        </div>
-        <button className="mt-5 w-full bg-blue-500 text-white p-3 text-center rounded-xl mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
-          Checkout
-        </button>
-      </div>
-      <div className="bg-white overflow-hidden rounded-3xl shadow-xl group">
-        <div className="bg-blue-500 p-6 pb-14 ">
-          <span className="text-white text-2xl">Profile</span>
-        </div>
-        <div className="rounded-3xl p-6 relative -top-5 bg-white">
-          <div className="flex relative -top-16 items-end justify-between">
-            <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Orders</span>
-              <span className="font-medium">340</span>
-            </div>
-            <div className="h-24 w-24 bg-red-400 rounded-full group-hover:bg-green-500 transition-colors"></div>
-            <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Spent</span>
-              <span className="font-medium">$340</span>
-            </div>
-          </div>
-          <div className="relative flex flex-col items-center -mt-10 -mb-5">
-            <span className="text-lg font-medium">Tony Molloy</span>
-            <span className="text-sm text-gray-500">미국</span>
-          </div>
-        </div>
-      </div>
-
-      <div className="bg-white overflow-hidden rounded-3xl shadow-xl p-6">
-        <div className="flex mb-5 justify-between items-center">
-          <span>🛬</span>
-          <div className="space-x-3">
-            <span>😚 1.5</span>
-            <span className="shadow-xl p-2 rounded-md">💝</span>
-          </div>
-        </div>
-        <div className="bg-zinc-400 h-72 mb-5" />
-        <div className="flex flex-col">
-          <span className="font-medium text-xl">Swoon Lounge</span>
-          <span className="text-xs text-gray-500">Chair</span>
-          <div className="mt-3 mb-5 flex justify-between items-center">
-            <div className="space-x-2">
-              <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition"></button>
-              <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition"></button>
-              <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition"></button>
-            </div>
-            <div className="flex items-center space-x-5">
-              <button className="bg-blue-200 flex justify-center items-center aspect-square w-8 font-medium text-xl text-gray-500 rounded-lg">
-                -
-              </button>
-              <span>1</span>
-              <button className="bg-blue-200 flex justify-center items-center aspect-square w-8 font-medium text-xl text-gray-500 rounded-lg">
-                +
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="flex justify-between items-center">
-          <span className="font-medium text-2xl">$450</span>
-          <button className="bg-blue-500 text-center text-white p-3 rounded-lg py-2 px-8 text-sm">
-            Add to cart
-          </button>
-        </div>
-      </div>
-      <form action="" className="flex flex-col h-80 gap-5 bg-white focus-within:bg-red-50">
-        <input required className="peer bg-blue-500 invalid:bg-red-500 placeholder-shown:bg-green-500 required:border-2 border-yellow-500" type="text" />
-        <span className="hidden peer-invalid:block text-red-500">Invalid!</span>
-        <span className="hidden peer-valid:block text-teal-400">Valid!</span>
-      </form>
+    <div className="sm:bg-red-400 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-400 2xl:bg-pink-400">
+      <input
+        type="file"
+        className="file:transition-colors file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 file:hover:border file:cursor-pointer file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-500"
+      />
+      <p className="sm:hover:bg-pink-200 first-letter:text-7xl first-letter:hover:text-pink-500">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, atque sit
+        ad non sunt dolorem officiis accusamus voluptates corrupti quam!
+      </p>
     </div>
   );
 };
