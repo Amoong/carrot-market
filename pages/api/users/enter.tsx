fee0b959d51600cc209d844530b940a7fc2f5640
Amoong <moo1323@naver.com>
2022-11-14 21:47:09 +0900

    Token Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,20 +5,25 @@ import { NextApiRequest, NextApiResponse } from "next";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: Number(phone) } : { email };
-  let user;
 
-  user = await client.user.upsert({
-    where: {
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      payload: "1234",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "Anonymous",
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
 
-  console.log(user);
+  console.log(token);
 
   return res.status(200).end();
 }
