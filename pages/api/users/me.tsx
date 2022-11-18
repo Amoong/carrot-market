d8f2f0f1bf1d973130015c99f2fd8e827b9ce00f
Amoong <moo1323@naver.com>
2022-11-18 21:37:08 +0900

    Cleaning Code

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -1,21 +1,12 @@
-import { withIronSessionApiRoute } from "iron-session/next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
-
-declare module "iron-session" {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    };
-  }
-}
+import { withApiSession } from "@libs/server/withSession";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  console.log(req.session.user);
   const profile = await client.user.findUnique({
     where: { id: req.session.user?.id },
   });
@@ -25,8 +16,4 @@ async function handler(
   });
 }
 
-export default withIronSessionApiRoute(withHandler("GET", handler), {
-  cookieName: "carrotsession",
-  password:
-    "12351235123521354322345234dsdjf;23094jd;lsdj;l34jl;has89y945120348fd",
-});
+export default withApiSession(withHandler("GET", handler));
