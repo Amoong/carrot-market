8eb1bd13e5ff1df92e6770fb3458bc6b45dbee74
Amoong <moo1323@naver.com>
2023-07-17 20:52:19 +0900

    Serverless Sessions

--- pages/api/users/confirm.ts
+++ pages/api/users/confirm.ts
@@ -1,5 +1,7 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/client/client";
 
 async function handler(
   req: NextApiRequest,
@@ -7,8 +9,28 @@ async function handler(
 ) {
   const { token } = req.body;
 
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+
+  if (!exists) {
+    res.status(404).end();
+  }
+
+  console.log(exists);
+
+  req.session.user = {
+    id: exists?.userId,
+  };
+
+  await req.session.save();
+
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: "01234523452368943709837403325784323494",
+});
