1d07db086a3fb37e7a53a4f77a3c6a033ad6f8d2
Amoong <moo1323@naver.com>
2023-07-29 10:29:59 +0900

    useCoords

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -7,6 +7,7 @@ import useMutation from "@libs/client/useMutation";
 import { useEffect } from "react";
 import { Post } from "@prisma/client";
 import { useRouter } from "next/router";
+import useCoords from "@libs/client/useCoords";
 
 interface WriteForm {
   question: string;
@@ -18,6 +19,8 @@ interface WriteResponse {
 }
 
 const Write: NextPage = () => {
+  const { latitude, longitude } = useCoords();
+
   const router = useRouter();
   const { register, handleSubmit } = useForm<WriteForm>();
   const [post, { loading, data }] = useMutation<WriteResponse>("/api/posts");
@@ -27,7 +30,7 @@ const Write: NextPage = () => {
       return;
     }
 
-    post(_data);
+    post({ ..._data, latitude, longitude });
   };
 
   useEffect(() => {
