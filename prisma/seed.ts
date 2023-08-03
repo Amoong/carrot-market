12a1e113903aa02c6f49468afc8490cf5bf81f7a
Amoong <moo1323@naver.com>
2023-08-03 12:47:49 +0900

    Seeding

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+async function main() {
+  [...Array.from(Array(500).keys())].forEach(async (num) => {
+    await client.stream.create({
+      data: {
+        name: String(num),
+        description: String(num),
+        price: num,
+        user: {
+          connect: {
+            id: 1,
+          },
+        },
+      },
+    });
+  });
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(() => client.$disconnect());
