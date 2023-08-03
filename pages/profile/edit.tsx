f1c3d186b0d82b23bcdea3aeecf69c1ba2734060
Amoong <moo1323@naver.com>
2023-08-03 16:53:23 +0900

    Image Preview

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -15,18 +15,28 @@ interface EditProfileForm {
   name?: string;
   email?: string;
   phone?: string;
+  avatar?: FileList;
 }
 
 const EditProfile: NextPage = () => {
   const { user } = useUser();
   const [error, setError] = useState("");
+  const [avatarPreview, setAvatarPreview] = useState("");
 
   const [editProfile, { loading, data }] =
     useMutation<EditProfileResponse>("/api/users/me");
 
-  const { register, setValue, handleSubmit } = useForm<EditProfileForm>({
+  const { register, setValue, handleSubmit, watch } = useForm<EditProfileForm>({
     mode: "onChange",
   });
+
+  const avatar = watch("avatar");
+
+  useEffect(() => {
+    if (avatar && avatar.length > 0) {
+      setAvatarPreview(URL.createObjectURL(avatar[0]));
+    }
+  }, [avatar]);
 
   useEffect(() => {
     if (user?.email) {
@@ -69,13 +79,22 @@ const EditProfile: NextPage = () => {
     <Layout canGoBack title="Edit Profile">
       <form className="space-y-4 py-10 px-4" onSubmit={handleSubmit(onValid)}>
         <div className="flex items-center space-x-3">
-          <div className="h-14 w-14 rounded-full bg-slate-500" />
+          {avatarPreview ? (
+            <img
+              src={avatarPreview}
+              className="h-14 w-14 rounded-full bg-slate-500"
+            />
+          ) : (
+            <div className="h-14 w-14 rounded-full bg-slate-500" />
+          )}
+
           <label
             htmlFor="picture"
             className="cursor-pointer rounded-md border border-gray-300 py-2 px-3 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
           >
             Change
             <input
+              {...register("avatar")}
               id="picture"
               type="file"
               className="hidden"
