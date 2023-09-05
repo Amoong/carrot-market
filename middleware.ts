85b8128788b86c0c8cc7332566bb27d9ac27045f
Amoong <moo1323@naver.com>
2023-09-05 21:00:16 +0900

    Responses and Redirections

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,19 @@
-export function middleware() {
-  console.log("i'm middle ware");
+import { NextRequest, NextResponse, userAgent } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const agent = userAgent(req);
+
+  if (agent.isBot) {
+    req.nextUrl.pathname = "/no-bot";
+    return NextResponse.rewrite(req.nextUrl);
+  }
+
+  if (!req.url.includes("/enter") && !req.cookies.has("carrotsession")) {
+    req.nextUrl.pathname = "/enter";
+    return NextResponse.redirect(req.nextUrl);
+  }
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|favicon.ico).*)"],
+};
